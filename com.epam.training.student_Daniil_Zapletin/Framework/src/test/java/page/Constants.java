86d1f394028167ba3009df9b4127174f3f1fd4ae
zapletin-dk <119448739+zapletin-dk@users.noreply.github.com>
2023-06-01 04:52:00 +0300

    Update Constants.java

--- com.epam.training.student_Daniil_Zapletin/Framework/src/test/java/page/Constants.java
+++ com.epam.training.student_Daniil_Zapletin/Framework/src/test/java/page/Constants.java
@@ -46,7 +46,7 @@ public class Constants {
         public static final String MAIL_PAGE_URL = "https://yopmail.com/";
         public static final String GENERATE_NEW_EMAIL_LINK = "//h3[contains(text(),'Случайный адрес электронной почты')]";
         public static final String GENERATED_EMAIL = "//div[@id='egen']";
-        public static final String CHECK_MAIL_BOX_BUTTON = "//button[@onclick='egengo()']";
+        public static final String CHECK_MAIL_BOX_BUTTON = "//button[@onclick='egengo();']";
         public static final String TOTAL_ESTIMATED_COST = "//td/h3[contains(text(), 'USD')]";
     }
 }
